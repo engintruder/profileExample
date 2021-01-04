d097acea655fa4b676ae9eddb451ff24db26ced8
engintruder <engkwangseob@gmail.com>
2021-01-04 23:58:12 +0900

    first commit

---
+++ src/main/java/com/intruder/project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.intruder.project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProjectApplication.class);
+    }
+
+}
